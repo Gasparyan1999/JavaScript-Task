77abfc563e24c91196e51cf4795134d2cc35d69e
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-17 16:29:38 +0400

    New Task

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// const num = prompt("Give me num");
-
-// function StringChallenge(num) {
-//   const hours = Math.floor(+num / 60);
-//   const minutes = +num - hours * 60;
-//   const result = `${hours}:${minutes}`;
-//   const ChallengeToken = "gvthuljp21da";
-//   const newString = result + ChallengeToken;
-//   let finalyStr = "";
-//   let count = 0;
-//   for (let i = 0; i < newString.length; i++) {
-//     count++;
-//     if (count == 4) {
-//       finalyStr += "_";
-//       count = 0;
-//     } else finalyStr += newString[i];
-//   }
-//   console.log(finalyStr);
-//   return (xun = 254);
-// }
-
-// StringChallenge(num);
-
 let str = prompt("Password");
 
 function StringChallenge(str) {
