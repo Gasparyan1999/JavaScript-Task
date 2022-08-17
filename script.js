13db45c35dcc1a6412004d5aa647ba3bf13d1609
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-17 18:04:35 +0400

    new fix

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let str = prompt("Password");
 
 function StringChallenge(str) {
-  if (
+  const passwordCheck =
     !str.toLowerCase().includes("password") &&
     str.replace(/\D/g, "").length > 0 &&
     str.length < 31 &&
     str.length > 7 &&
     str.replace(/[^a-z]/, "").replace(/\D/g, "").length > 0 &&
-    str.toLowerCase() != str
-  ) {
-    return console.log(true);
-  } else {
-    return console.log(false);
-  }
+    str.toLowerCase() != str;
+
+    return console.log(passwordCheck)
 }
 
 StringChallenge(str);
