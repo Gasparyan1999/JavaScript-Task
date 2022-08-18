650b1259cf67d9a880c2c4ee89114d1fc486b0d0
Gasparyan1999 <gasparyan.george1999@gmail.com>
2022-08-18 10:39:13 +0400

    Price

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-let str = prompt("Password");
+const myPriceArr = [44, 30, 24, 3000, 35, 30, 40, 38, 15];
+const ChallengeToken = "gvthuljp21da";
 
-function StringChallenge(str) {
-  const passwordCheck =
-    !str.toLowerCase().includes("password") &&
-    str.replace(/\D/g, "").length > 0 &&
-    str.length < 31 &&
-    str.length > 7 &&
-    str.replace(/[^a-z]/, "").replace(/\D/g, "").length > 0 &&
-    str.toLowerCase() != str;
-
-    return console.log(passwordCheck)
+function ArrayChallenge(arr) {
+  let result = 0;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] - arr[count] > result) result = arr[i] - arr[count];
+    if (i == arr.length - 1) {
+      count++;
+      i = count;
+    }
+  }
+  if (result > 0) return result;
+  else return -1;
 }
 
-StringChallenge(str);
+const newStr = ChallengeToken + ArrayChallenge(myPriceArr);
+
+let newCount = 0;
+let overStr = "";
+for (let i = 0; i < newStr.length; i++) {
+  if (newCount == 3) {
+    overStr += "_";
+    newCount = 0;
+  } else {
+    overStr += newStr[i];
+    newCount++;
+  }
+}
+
+console.log(overStr);
